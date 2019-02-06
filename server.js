8989938ea1b5cc762c9d1020cffd4f831c27ab1b
Errol <errolf07@gmail.com>
2019-02-06 20:31:39 +0530

    UI Fixed
    Signed-off-by: Errol <errolf07@gmail.com>

--- server.js
+++ server.js
@@ -6,16 +6,17 @@ const api = require('./server/routes/api');
 const port = 3000;
 const app = express();
 
-//app.use(express.static(path.join(__dirname,'/dist/App1/')));
+app.use(express.static(path.join(__dirname,'/dist/App1/')));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use('/api',api);
+//app.use('/api',api);
 
 // app.get('*',(req,res) => {
 // 	res.sendFile(path.join(__dirname,'/dist/App1/index.html'));
 // });
 app.get('*',(req,res) => {
-	res.send('api works1');
+    //res.send('api works1');
+    res.sendFile(path.join(__dirname,'/dist/App1/index.html'));
 });
 
 app.listen(port,() => {
